dfc98f3dc17ffc447d9a2e40d1491d4ff75b383b
LuanSilva9 <luanpdev@gmail.com>
2024-10-24 21:47:30 -0400

    Adicionando mais conteudo

--- src/routes/client/Home/Home.jsx
+++ src/routes/client/Home/Home.jsx
@@ -1,3 +1,4 @@
+import { Button } from 'react-bootstrap'
 import './Home.css'
 
 
@@ -7,7 +8,15 @@ export default function Home() {
             <section className='hero-section'>
                 <h2>Estacione com Facilidade e Segurança</h2>
                 <p>Reserve sua vaga antecipadamente, faça check-in automático e aproveite a conveniência do Estacionejá. Simples, rápido e eficiente!</p>
-            
+
+                <Button className='btn btn-primary' href="#saiba-mais">
+                    Saiba mais
+                </Button>
+            </section>
+
+            <section className="saiba-mais" id="saiba-mais">
+                <h2>Saiba Mais!</h2>
+                <p>O EstacioneJa é um software de gerenciamento de Estacionamentos para Empresas e Universidades, você pode reservar uma vaga!</p>
             </section>
         </main>
     )
