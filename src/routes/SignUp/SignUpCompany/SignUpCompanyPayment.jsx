cededc43451a5388f02c93a7c9fc9da26d1e2be3
Luan <luanpdev@gmail.com>
2024-11-21 20:42:56 +0000

    finalizando o passo 3

--- src/routes/SignUp/SignUpCompany/SignUpCompanyPayment.jsx
+++ src/routes/SignUp/SignUpCompany/SignUpCompanyPayment.jsx
@@ -5,6 +5,7 @@ import '../SignUp.css';
 import { Link } from "react-router-dom";
 
 import { MdNavigateNext } from "react-icons/md";
+import { useState } from "react";
 
 const styleCategory = {
     border: "1px solid rgb(54 183 123)",
@@ -26,54 +27,54 @@ export default function SignUpCompanyPayment() {
                         <img src="/logo-white.svg" alt="logo-estacioneja" className="logo" />
                         <h2>Adicionar forma de pagamento</h2>
                     </div>
-                    
+
                     <Grid container spacing={2} sx={{ mb: 1 }}>
                         <Grid item xs={12}>
                             <Typography textAlign={"start"} sx={{ fontSize: "17px", fontWeight: "bold" }}>Selecione a forma de pagamento</Typography>
                         </Grid>
 
-                        <Grid item xs={12} key={1} onClick={() => handleMarkPayments(1)} style={selectedCategory === 1 ? styleCategory : null}>
-                            <Grid item xs={6}>
-                                <Typography sx={{fontWeight: 'bold'}}>Pacote Super</Typography>
-                                <Typography>Esse pacote é bom para estacionamentos grandes (+ 100 vagas)</Typography>
-                            </Grid>
-                            
-                            <Grid item xs={6} key={2}>
-                                <Typography sx={{fontWeight: 'bold'}}>1300,00R$</Typography>
-                                <Typography sx={{fontWeight: 'bold'}}>Instalação + Suporte</Typography>
-                            </Grid>
-                        </Grid>
+                        <div className="paymentMethods" onClick={() => handleMarkPayments(1)} style={selectedCategory === 1 ? styleCategory : null}>
+                            <div className="textLefts">
+                                <h4>Pacote Super</h4>
+                                <p>Esse pacote é bom para estacionamentos grandes (+ 100 vagas)</p>
+                            </div>
 
-                        <Grid item xs={12} key={2} onClick={() => handleMarkPayments(2)} style={selectedCategory === 2 ? styleCategory : null}>
-                            <Grid item xs={6}>
-                                <Typography sx={{fontWeight: 'bold'}}>Pacote Medio</Typography>
-                                <Typography>Feito para estacionamentos de até 70 vagas</Typography>
-                            </Grid>
-                            
-                            <Grid item xs={6}>
-                                <Typography sx={{fontWeight: 'bold'}}>1000,00R$</Typography>
-                                <Typography sx={{fontWeight: 'bold'}}>Instalação + Suporte</Typography>
-                            </Grid>
-                        </Grid>
-                        
-                        <Grid item xs={12} key={3} onClick={() => handleMarkPayments(3)} style={selectedCategory === 3 ? styleCategory : null}>
-                            <Grid item xs={6}>
-                                <Typography sx={{fontWeight: 'bold'}}>Pacote Minimo</Typography>
-                                <Typography>Feito para estacionamentos de até 30 vagas</Typography>
-                            </Grid>
-                            
-                            <Grid item xs={6}>
-                                <Typography sx={{fontWeight: 'bold'}}>400,00R$</Typography>
-                                <Typography sx={{fontWeight: 'bold'}}>Instalação + Suporte</Typography>
-                            </Grid>
-                        </Grid>
+                            <div className="textRight">
+                                <h4>1300,00R$</h4>
+                                <p>Instalação + Suporte</p>
+                            </div>
+                        </div>
 
-                       
+                        <div className="paymentMethods" onClick={() => handleMarkPayments(2)} style={selectedCategory === 2 ? styleCategory : null}>
+                            <div className="textLefts">
+                                <h4>Pacote Medio</h4>
+                                <p>Feito para estacionamentos de até 70 vagas</p>
+                            </div>
+
+                            <div className="textRight">
+                                <h4>1000,00R$</h4>
+                                <p>Instalação + Suporte</p>
+                            </div>
+                        </div>
+
+                        <div className="paymentMethods" onClick={() => handleMarkPayments(3)} style={selectedCategory === 3 ? styleCategory : null}>
+                            <div className="textLefts">
+                                <h4>Pacote Minimo</h4>
+                                <p>Feito para estacionamentos de até 30 vagas</p>
+                            </div>
+
+                            <div className="textRight">
+                                <h4>400,00R$</h4>
+                                <p>Instalação + Suporte</p>
+                            </div>
+                        </div>
+
+
 
                         <Grid item xs={12} sx={{ mt: 3 }}>
                             <Box display="flex" justifyContent="space-between">
-                                <Link to="/cadastrar/empresa/passo-2"  className="btn btn-dark">Voltar</Link>
-                                <Link to="/cadastrar/empresa/finish" className="btn btn-primary">Proximo Passo <MdNavigateNext/></Link>
+                                <Link to="/cadastrar/empresa/passo-2" className="btn btn-dark">Voltar</Link>
+                                <Link to="/cadastrar/empresa/finish" className="btn btn-primary">Proximo Passo <MdNavigateNext /></Link>
                             </Box>
                         </Grid>
                     </Grid>
