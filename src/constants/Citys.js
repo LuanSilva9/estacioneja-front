c3accb2fc8d0f9af67b2b8cc99149c0fbd60135d
LuanSilva9 <luanpdev@gmail.com>
2024-11-24 22:18:00 -0400

    Adicionando Cidades

---
+++ src/constants/Citys.js
@@ -0,0 +1,26 @@
+const citys = [
+    {
+        name: "Navirai",
+        cep: "79950000",
+        uf: "MS"
+    },
+    {
+        name: "Campo Grande",
+        cep: "79000000",
+        uf: "MS"
+    },
+    {
+        name: "Dourados",
+        cep: "79800000",
+        uf: "MS"
+    },
+    {
+        name: "Sao paulo",
+        cep: "01153000",
+        uf: "SP"
+    }
+];
+
+const ufs = ["AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO"];
+
+export { citys, ufs };
