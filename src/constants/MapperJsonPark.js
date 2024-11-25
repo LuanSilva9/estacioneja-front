cffde4e8ddaa79beedd5cac5c30bcfd6762abec9
LuanSilva9 <luanpdev@gmail.com>
2024-11-24 22:18:36 -0400

    Alterando Mapper

--- src/constants/MapperJsonPark.js
+++ src/constants/MapperJsonPark.js
@@ -3,7 +3,7 @@ import { companys } from "./Companys";
 const park = {
     companyName: companys[0].companyName,
     parkSlots: 50,
-    pathPark: `/${companys[0].companyNameFormated}/painel`
+    pathPark: `${companys[0].companyNameFormated}/painel`
 }
 
 export {park}
