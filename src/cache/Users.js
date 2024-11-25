9f567ae8052b67460aa3025ad86c061b231214c6
LuanSilva9 <luanpdev@gmail.com>
2024-11-25 18:09:18 -0400

    simulando estrutura de dados em banco com objeto JSON

--- src/cache/Users.js
+++ src/cache/Users.js
@@ -1,5 +1,45 @@
 var users = [
-
+    {
+        "userName": "Luan Freire Alves da Silva",
+        "userEmail": "estacioneja@admin.com",
+        "userPassword": "12345",
+        "userImage": "/assets/images.png",
+        "userCity": {
+            "name": "Naviraí",
+            "uf": "MS",
+            "cep": "79950-000"
+        },
+        "userPhone": "(11) 99999-9999",
+        "userCompanyVincles": [
+            {
+                "companyName": "SESC",
+                "companyNameFormated": "sesc",
+                "companyLogo": '/assets/camera.svg',
+                "companyCity": {
+                    "name": "Campo Grande",
+                    "cep": "79000000",
+                    "uf": "MS"
+                
+                },
+                "companyRepresentative": "Carlos Alberto",
+                "companyParkImageMap": '/assets/ParkMapperImg.svg',
+                "companyAddress": {
+                    "street": 'Av. Afonso Pena, 4321',
+                    "neighborhood": 'Amambaí'
+                },
+                "companyCnpj": '04236718000192',
+                "companyType": "EMPRESA",
+                "companyStructsInstalled": true,
+                "companyParkSlots": 150,
+                "companyParkSlotsFilled": 0,
+                "companyAcess": {
+                    "email": 'contato@sesc.com.br',
+                    "password": 'senha1011'
+                }
+            }
+        ],
+        "userReservation": []
+    }
 ]
 
 export {users};
