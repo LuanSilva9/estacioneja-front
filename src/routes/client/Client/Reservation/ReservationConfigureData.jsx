fbbd50912f91578c089ff77194449fb0033d99e0
LuanSilva9 <luanpdev@gmail.com>
2025-01-17 19:14:29 -0300

    Consertando problema do objeto referente aos veiculos vinculados ao usuario

--- src/routes/client/Client/Reservation/ReservationConfigureData.jsx
+++ src/routes/client/Client/Reservation/ReservationConfigureData.jsx
@@ -1,10 +1,12 @@
 import { Autocomplete, Grid, TextField  } from "@mui/material";
 import { useReservation } from "../../../../contexts/ReservationContext"
 import { Link } from "react-router-dom";
+import { useUser } from "../../../../contexts/UserContext";
 
 
 export default function ReservationConfigureData() {
     const { reservationData, updateReservationData } = useReservation();
+    const { userData, updateUserData } = useUser();
 
     function getOptionByDay() {
         const date = new Date();
@@ -19,16 +21,36 @@ export default function ReservationConfigureData() {
 
         return options;
     }
+
+    function getVeiclesName() {
+        const veicles = userData.userVeicles;
+
+        let veiclesName = [];
+
+        if(veicles) {
+            for(let i = 0; i < veicles.length; i++) {
+                veiclesName.push(`${veicles[i].model}, ${veicles[i].name}`)
+
+                
+            }
+
+            return veiclesName;
+        } else {
+            return ["Nao foi possivel encontrar"]
+        }
+    }
     
     return (
         <Grid container spacing={2}>
             <Grid item xs={12}>
-                <TextField 
+                <Autocomplete
                     fullWidth
-                    label="Selecione qual veiculo irá entrar"
+                    options={getVeiclesName()}
+                    getOptionLabel={(option) => option}
                     value={reservationData.reservationVeicle}
-                    onChange={(e) => {
-                        updateReservationData({ ...reservationData, reservationVeicle: e.target.value })
+                    renderInput={(params) => <TextField {...params} label="Selecione o Veiculo que irá entrar" variant="outlined" fullWidth />}
+                    onChange={(e, newValue) => {
+                        updateReservationData({ ...reservationData, reservationVeicle: newValue })
                     }}
                 />
             </Grid>
