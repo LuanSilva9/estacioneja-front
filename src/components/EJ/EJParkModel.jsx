dd5a7e2815dc78cd2bfcdcc9d93786d94a149a24
LuanSilva9 <luanpdev@gmail.com>
2025-01-21 16:43:47 -0300

    Alterando Layout para deixar mais intuitivo

--- src/components/EJ/EJParkModel.jsx
+++ src/components/EJ/EJParkModel.jsx
@@ -1,12 +1,51 @@
-import { Button } from "react-bootstrap";
+import * as React from 'react';
+
+import { Button, Typography } from "@mui/material";
+
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import { DEFAULT_LOGO } from '../../defaultValues';
+import { useUser } from '../../contexts/UserContext';
+import { ImBlocked } from "react-icons/im";
 
 export default function EJParkModel({ MapperJsonPark }) {
-    return (
-        <div className="park">
-            <h3>{MapperJsonPark.companyName}</h3>
-            <p>{MapperJsonPark.companyParkSlots} vagas disponiveis</p>
+    const { userData, updateUserData } = useUser();
 
-            <Button variant="dark" href={`/client/estacionamento/${MapperJsonPark.companyNameFormated}`}>Acessar Estacionamento</Button>
-        </div>
+    function desvincularEstacionamento() {
+
+    }
+
+    function generateBackgroundForCards() {
+        if(MapperJsonPark.companyLogo == DEFAULT_LOGO) {
+            return { height: 140, background: '#ccc' }
+        }
+        else {
+            return { height: 140, background: `url(${MapperJsonPark.companyLogo}) no-repeat center center`, backgroundSize: '30%' }
+        }
+    }
+
+    console.log(userData)
+
+    return (
+        <Card sx={{ maxWidth: "588px", border: '1px solid #3333' }}>
+            <CardMedia
+            sx={generateBackgroundForCards()}
+            />
+            <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+                {MapperJsonPark.companyName}
+            </Typography>
+            <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                Deseja estacionar aqui? no momento esse estacionamento está com <b>{MapperJsonPark.companyParkSlots}</b> vagas disponiveis
+            </Typography>
+            </CardContent>
+            <CardActions sx={{ display: 'flex', flexWrap: 'wrap', gap: 2 }}>
+                <Button href={`/client/estacionamento/${MapperJsonPark.companyNameFormated}`} variant='contained' color='success'>Acessar Estacionamento</Button>
+                <Button variant='outlined' color='error' sx={{margin: "0 !important"}} onClick={desvincularEstacionamento} startIcon={<ImBlocked/>}>Desvincular</Button>
+            </CardActions>
+        </Card>
+
     )
 }
--- src/components/EJ/EJVinclePark.jsx
+++ src/components/EJ/EJVinclePark.jsx
@@ -3,5 +3,5 @@ import { Link } from "react-router-dom";
 import { FaPlus } from "react-icons/fa";
 
 export default function EJVinclePark() {
-    return <Link className="vincle rounded-0" to={`/client/pesquisar-estacionamentos`}><FaPlus/></Link>;
+    return <Link className="vincle rounded-0" to={`/client/pesquisar-estacionamentos`}>Deseja adicionar mais um estacionamento? clique aqui!</Link>;
 }
