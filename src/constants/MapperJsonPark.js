a8555aac32b914f3e20832ba779199eb643503a4
LuanSilva9 <luanpdev@gmail.com>
2025-01-21 16:43:03 -0300

    Apagando mappers

--- src/constants/MapperJsonPark.js
+++
@@ -1,9 +0,0 @@
-import { companys } from "./Companys";
-
-const park = {
-    companyName: companys[0].companyName,
-    parkSlots: 50,
-    pathPark: `${companys[0].companyNameFormated}/painel`
-}
-
-export {park}
