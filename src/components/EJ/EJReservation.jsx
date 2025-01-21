532435024e90cbe5dbad54128685f323c86cb0ba
LuanSilva9 <luanpdev@gmail.com>
2025-01-21 16:42:35 -0300

    Alterando a interface de EJReservation: Deixando mais intuitivo

--- src/components/EJ/EJReservation.jsx
+++ src/components/EJ/EJReservation.jsx
@@ -1,33 +1,67 @@
-import { Avatar, Divider } from "@mui/material";
+import * as React from 'react';
+
+import { Box, Divider, Typography } from "@mui/material";
 import { Button } from "react-bootstrap";
 
-export default function EJReservation({ MapperJsonReservation }) {
+import { IoMdNavigate } from "react-icons/io";
+import { IoExit } from "react-icons/io5"
+import { FaIdCardClip } from "react-icons/fa6";
+import { FaCar } from "react-icons/fa";
+import dayjs from 'dayjs';
+
+
+export default function EJReservation({ MapperJsonReservation, onlyInfo }) {
+    // function getTimeByReservation() {
+    //     const time = new Date();
+    //     const timeRequired = MapperJsonReservation.comeTime;
+    // }
+
     return (
-        <section className="reservation">
-            <div className="painel-title">
-                <h2>Painel de Reserva</h2>
-                
-                <Avatar sx={{ width: 32, height: 32 }} src="/assets/camera" />
-            </div>
-            <Divider/>
+        <section className="reservation" style={{ borderTop: "2px solid green" }}>
+            {
+                !onlyInfo ? (
+                    <>
+                        <Typography align='center' variant="h5" component="div">Olá, Você possui uma Reserva</Typography>
 
-            <div className="control">
-                <div className="painel">
-                    <h4>Voce possui umas reserva feita para: {MapperJsonReservation.companyName}</h4>
-                    <h4>Horario de chegada & saida: {MapperJsonReservation.comeTime}-{MapperJsonReservation.finishTime}h</h4>
-                    <h4>Placa do veiculo Cadastrada: {MapperJsonReservation.veicle}</h4>
-                </div>
+                        <Divider>Informações</Divider>
+                    </>
+                )
+                            : null
+            }
+            <Box className="control">
+                <Box className="card-info-single" sx={{ bgcolor: 'primary.light', color: 'white' }}>
+                    <Typography align='center' variant="h6">Local</Typography>
+                    <IoMdNavigate className='card-info-icon'/>
+                    <Typography align='center' variant='body3'>{MapperJsonReservation.reservationPark.companyName}</Typography>
+                </Box>
 
-                <div className="infoCompany">
-                    
-                </div>
-            </div>
+                <Box className="card-info-single" sx={{ bgcolor: 'success.light', color: 'white' }}>
+                    <Typography align='center' variant="h6">Horario Chegada</Typography>
+                    <FaIdCardClip className='card-info-icon' />
+                    <Typography align='center' variant='body3'>{dayjs(MapperJsonReservation.reservationDateEntry).isValid() ? dayjs(MapperJsonReservation.reservationDateEntry).format("hh") : null}h</Typography>
+                </Box>
 
-            <div className="btnControl">
-                <Button variant="light" className="rounded border border-1">Fazer Check-in</Button>
-                <Button variant="dark" className="rounded">Gerar PDF</Button>
-            </div>
+                <Box className="card-info-single" sx={{ bgcolor: 'error.light', color: 'white' }}>
+                    <Typography align='center' variant="h6">Horario Saida</Typography>
+                    <IoExit className='card-info-icon'/>
+                    <Typography align='center' variant='body3'>{ dayjs(MapperJsonReservation.reservationDateExit).isValid() ? dayjs(MapperJsonReservation.reservationDateExit).format("hh") : null }h</Typography>
+                </Box>
 
+                <Box className="card-info-single" sx={{ bgcolor: 'primary.main', color: 'white' }}>
+                    <Typography align='center' variant="h6">Veiculo</Typography>
+                    <FaCar className='card-info-icon'/>
+                    <Typography align='center' variant='body3'>{MapperJsonReservation.reservationVeicle}</Typography>
+                </Box>
+            </Box>
+
+            {
+                !onlyInfo ? (
+                    <div className="btnControl">
+                        <Button variant="dark" className="rounded">Gerar PDF</Button>
+                        <Button variant="success" className="rounded border border-1">Fazer Check-in</Button>
+                    </div>
+                ) : null
+            }
         </section>
     )
 }
