4ea09df0e7cdf49e534c721367ee2b64b25bbc81
LuanSilva9 <luanpdev@gmail.com>
2025-02-03 13:05:01 -0300

    wknd Feat: Melhorando interface de cliente logado (Mais intuitivo)

--- src/routes/client/Client/ClientApp.jsx
+++ src/routes/client/Client/ClientApp.jsx
@@ -20,16 +20,28 @@ import Box from '@mui/material/Box';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 
+import { MdOutlineAddCircle } from "react-icons/md";
+
 import NoLinkedParking from '../../../components/errorPages/NoLinkedParking.jsx';
+import { useNavigate } from 'react-router-dom';
+import Error404 from '../../../components/errorPages/Error404.jsx';
 
 export default function ClientApp() {
     const { userData, updateUserData } = useUser();
 
-    const [value, setValue] = React.useState(0);
+    const navigate = useNavigate();
 
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
+    const [valueReservation, setValueReservation] = React.useState(0);
+    const [valueVincle, setValueVincle] = React.useState(0);
+
+    const handleChangeReservation = (event, newValue) => {
+        setValueReservation(newValue);
     };
+
+    const handleChangeVincle = (event, newValue) => {
+        setValueVincle(newValue);
+    };
+
 
     if(localStorage.getItem('parkSelected')) {
         localStorage.removeItem('parkSelected');
@@ -48,14 +60,14 @@ export default function ClientApp() {
                     <Box sx={{ width: '100%', bgcolor: 'background.paper' }}>
                         <Typography align='center' variant="h4" component="div" sx={{ mt: 5, mb: 3}}>Olá, Confira suas reservas!</Typography>
 
-                        <Tabs value={value} onChange={handleChange} centered TabIndicatorProps={{style: { backgroundColor: "rgb(0, 100, 0)"}}}>
+                        <Tabs value={valueReservation} onChange={handleChangeReservation} centered TabIndicatorProps={{style: { backgroundColor: "rgb(0, 100, 0)"}}}>
                             {userData.userReservation.map((reservation, index) => (
-                                <Tab key={index} label={<span style={{ color: "rgb(0, 00, 0)", fontWeight: 500 }}><MdCorporateFare/> {reservation.reservationPark.companyName}</span>} />
+                                <Tab key={index} label={<span style={{ color: "rgb(0, 00, 0)", fontWeight: 500 }}>Dia {reservation.reservationDay}</span>} />
                             ))}
                         </Tabs>
                         
-                        {userData.userReservation[value] && (
-                            <EJReservation MapperJsonReservation={userData.userReservation[value]} />
+                        {userData.userReservation[valueReservation] && (
+                            <EJReservation MapperJsonReservation={userData.userReservation[valueReservation]} />
                         )}
                     </Box>
                 ) : null
@@ -67,25 +79,40 @@ export default function ClientApp() {
 
                 <div className="cardsParks">
                     {
-                        userData.userCompanyVincles ? userData.userCompanyVincles.map((park, i) => <EJParkModel key={i} MapperJsonPark={park} />) : null
-                    }
+                        userData.userCompanyVincles.length != 0 ? (
+                            <Box sx={{ width: '100%', bgcolor: 'background.paper' }}>
+                                <Tabs value={valueVincle} onChange={handleChangeVincle} sx={{ mb:2 }} centered TabIndicatorProps={{style: { backgroundColor: "rgb(0, 100, 0)"}}}>
+                                    {
+                                        userData.userCompanyVincles.map((company, index) => (
+                                            <Tab key={index} label={<span style={{ color: "rgb(0, 00, 0)", fontWeight: 500 }}>{index + 1}</span>}/>
+                                        )) 
+                                    }
 
-                    {
-                        userData.userCompanyVincles.length == 0 ? <NoLinkedParking /> : null
+                                        <Tab onClick={() => navigate("pesquisar-estacionamentos")} label={<span style={{fontSize: '20px'}}><MdOutlineAddCircle/></span>}/>
+                                </Tabs>
+
+                                {userData.userCompanyVincles[valueVincle] && (
+                                    <EJParkModel MapperJsonPark={userData.userCompanyVincles[valueVincle]} />
+                                )}
+                            </Box>
+                        ) : <NoLinkedParking />
                     }
                 </div>
             </div>
 
-            <div className="historys">
-                <Typography align="center" variant="h4">Historico de Reservas</Typography>
+            {
+                userData.userReservationHistory.length != 0 ? (
+                    <div className="historys">
+                        <Typography align="center" variant="h4">Historico de Reservas</Typography>
 
-                <div className="cardsHistorys">
-                    <EJHistory MapperJsonHistory={history} />
-                    <EJHistory MapperJsonHistory={history} />
-                    <EJHistory MapperJsonHistory={history} />
-                    <EJHistory MapperJsonHistory={history} />
-                </div>
-            </div>
+                        <div className="cardsHistorys">
+                            {
+                                userData.userReservationHistory.map((history, index) => <EJHistory key={index} MapperJsonHistory={history}/>)
+                            }
+                        </div>
+                    </div>
+                ) : null
+            } 
 
             <FooterClient />
         </main>
