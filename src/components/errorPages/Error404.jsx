2c8d0abf7df392020ef2f59985e392f49ae12c66
LuanSilva9 <luanpdev@gmail.com>
2025-02-03 13:04:05 -0300

    wknd Feat: Modelando páginas de erros personalizados (Beta)

--- src/components/errorPages/Error404.jsx
+++ src/components/errorPages/Error404.jsx
@@ -1,13 +1,14 @@
 import './errorStyles.css';
 import '../../constants/pallete.css';
 
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 
 export default function Error404() {
     return (
-        <Box className='not-park-vincle'>
+        <Box className='box-error'>
             <Typography align='center' variant='h5'>Erro 404 - Página não encontrada</Typography>
             <img src="/assets/parking-not-vincle.svg" />
+            <Button variant='contained' color='error' href={"/"}>Voltar</Button>
         </Box> 
     )
 }
--- src/components/errorPages/NoLinkedParking.jsx
+++ src/components/errorPages/NoLinkedParking.jsx
@@ -1,13 +1,15 @@
 import './errorStyles.css'
 import '../../constants/pallete.css'
 
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
+import { rootColors } from '../../constants/pallete';
 
 export default function NoLinkedParking() {
     return (
-        <Box className='not-park-vincle'>
+        <Box className='box-error'>
             <Typography align='center' variant='h5'>Você não tem nenhum estacionamento vinculado!</Typography>
             <img src="/assets/parking-not-vincle.svg" />
+            <Button href='/client/pesquisar-estacionamentos' variant='contained' sx={{ background: rootColors.colorGreenPrimary }}>Clique aqui para ir para a página de vinculos</Button>
         </Box> 
     )
 }
