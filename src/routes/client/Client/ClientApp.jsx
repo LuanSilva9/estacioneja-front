ff915989af4d697ba540c144a0cbf226d5195ff0
LuanAlves17 <luan.alves@copasul.coop.br>
2025-04-22 13:15:34 -0300

    Deixando o client home mais intuitivo

--- src/routes/client/Client/ClientApp.jsx
+++ src/routes/client/Client/ClientApp.jsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 import './Client.css'
 
-import { Toolbar, Typography } from "@mui/material";
+import { Button, IconButton, Snackbar, Toolbar, Typography, useMediaQuery } from "@mui/material";
 
 import EJParkModel from "../../../components/EJ/EJParkModel";
 import EJReservation from "../../../components/EJ/EJReservation";
@@ -15,30 +15,45 @@ import Box from '@mui/material/Box';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 
-import AppBar from '@mui/material/AppBar';
-import CssBaseline from '@mui/material/CssBaseline';
-import Divider from '@mui/material/Divider';
-import Drawer from '@mui/material/Drawer';
-import IconButton from '@mui/material/IconButton';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-
 import NoLinkedParking from '../../../components/errorPages/NoLinkedParking.jsx';
 import { useNavigate } from 'react-router-dom';
 import { FaSearch } from 'react-icons/fa';
 import { rootColors } from '../../../constants/pallete.js';
-
-const drawerWidth = 340;
+import { GiNextButton } from 'react-icons/gi';
+import { BsBack } from 'react-icons/bs';
+import { FaBackwardStep } from 'react-icons/fa6';
+import { useTheme } from '@mui/material/styles';
+import { MdNavigateNext, MdOutlineArrowBackIosNew } from 'react-icons/md';
+import { IoChevronBack } from 'react-icons/io5';
+import { GrNext } from 'react-icons/gr';
 
 export default function ClientApp(props) {
     const { userData, updateUserData } = useUser();
     
-    const navigate = useNavigate();
-    
     const [valueReservation, setValueReservation] = React.useState(0);
     const [valueVincle, setValueVincle] = React.useState(0);
+    const [openSnackbar, setOpenSnackbar] = React.useState(false);
+    
+    const scrollRef = React.useRef();
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
+    const scroll = (direction) => {
+        if (scrollRef.current) {
+            scrollRef.current.scrollBy({
+                left: direction === "left" ? -500 : 500,
+                behavior: "smooth",
+            });
+        }
+    };
+    
+    const handleClickSnackbar = () => {
+        setOpenSnackbar(true);
+    };
+    
+    const handleCloseSnackbar = () => {
+        setOpenSnackbar(false);
+    };
     
     const handleChangeReservation = (event, newValue) => {
         setValueReservation(newValue);
@@ -47,6 +62,13 @@ export default function ClientApp(props) {
     const handleChangeVincle = (event, newValue) => {
         setValueVincle(newValue);
     };
+
+
+    const action = (
+        <Button color="primary" size="small" onClick={handleCloseSnackbar}>
+            OK
+        </Button>
+    );
 
 
     if(localStorage.getItem('parkSelected')) {
@@ -82,27 +104,61 @@ export default function ClientApp(props) {
                         <div className="parkVincles">
                             <Typography align="center" variant="h4">Estacionamentos Vinculados </Typography>
 
-                            <div className="cardsParks">
-                                {
-                                    userData.userCompanyVincles.length != 0 ? (
-                                        <Box sx={{ width: '100%', bgcolor: 'background.paper' }}>
-                                            <Tabs value={valueVincle} onChange={handleChangeVincle} sx={{ mb:2 }} centered TabIndicatorProps={{style: { backgroundColor: "rgb(0, 100, 0)"}}}>
-                                                {
-                                                    userData.userCompanyVincles.map((company, index) => (
-                                                        <Tab key={index} label={<span style={{ color: "rgb(0, 00, 0)", fontWeight: 500 }}>{index + 1}</span>}/>
-                                                    )) 
-                                                }
+                            {
+                                userData.userCompanyVincles.length != 0 ? 
+                                <Box sx={{ display: "flex", alignItems: "center", width: "100%", position: "relative" }}>
+                                    <IconButton
+                                        onClick={() => scroll("left")}
+                                        sx={{ position: "absolute", left: 5, zIndex: 1, backgroundColor: "rgba(255, 255, 255, 0.7)", borderRadius: 2 }}
+                                    >
+                                        <MdOutlineArrowBackIosNew />
 
-                                                    <Tab onClick={() => navigate("pesquisar-estacionamentos")} label={<span style={{fontSize: '20px', color: rootColors.colorGreenShade}}><FaSearch /></span>}/>
-                                            </Tabs>
+                                    </IconButton>
 
-                                            {userData.userCompanyVincles[valueVincle] && (
-                                                <EJParkModel MapperJsonPark={userData.userCompanyVincles[valueVincle]} />
-                                            )}
-                                        </Box>
-                                    ) : <NoLinkedParking />
-                                }
-                            </div>
+                                    <Box
+                                        ref={scrollRef}
+                                        sx={{
+                                            display: "flex",
+                                            overflowX: "auto",
+                                            scrollSnapType: "x mandatory",
+                                            gap: 4,
+                                            px: 6,
+                                            py: 2,
+                                            width: "100%",
+                                            scrollbarWidth: "none",
+                                            "&::-webkit-scrollbar": { display: "none" },
+                                        }}
+                                    >
+                                        {userData.userCompanyVincles.map((company, index) => (
+                                            <Box
+                                                key={index}
+                                                sx={{
+                                                    scrollSnapAlign: "center",
+                                                    flex: "0 0 auto",
+                                                    width: isMobile ? "90vw" : "100%", 
+                                                }}
+                                            >
+                                                <EJParkModel MapperJsonPark={company} exibirSnackbar={handleClickSnackbar} />
+                                            </Box>
+                                        ))}
+                                    </Box>
+
+                                    <IconButton
+                                        onClick={() => scroll("right")}
+                                        sx={{ position: "absolute", right: 5, zIndex: 1, backgroundColor: "rgba(255, 255, 255, 0.7)", borderRadius: 2 }}
+                                    >
+                                        <GrNext />
+                                    </IconButton>
+                                </Box> : <NoLinkedParking />
+                            }
+
+                            <Snackbar
+                                open={openSnackbar}
+                                onClose={handleCloseSnackbar}
+                                message="Estacionamento Desvinculado"
+                                action={action}
+                                autoHideDuration={3000}
+                            />
                         </div>
                 <FooterClient />
             </main>
