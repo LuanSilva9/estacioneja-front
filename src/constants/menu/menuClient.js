4b2a9b5d335bf7f7a764d01d94805dcdb94d79b4
LuanAlves17 <luan.alves@copasul.coop.br>
2025-04-22 13:15:03 -0300

    Melhorando Menu

--- src/constants/menu/menuClient.js
+++ src/constants/menu/menuClient.js
@@ -1,12 +1,23 @@
 import { FaRegUser } from "react-icons/fa6"
+import { HiFlag, HiHome } from "react-icons/hi"
 import { PiCarSimpleFill } from "react-icons/pi"
 import { RiSettingsFill } from "react-icons/ri"
 
 const menuClient = [
     {
+        label: 'Principal',
+        href: '/',
+        icon: HiHome
+    },
+    {
         label: 'Meus Veiculos',
         href: '/client/perfil/veiculos',
         icon: PiCarSimpleFill
+    },
+    {
+        label: 'Estacionamentos',
+        href: '/client/pesquisar-estacionamentos',
+        icon: HiFlag 
     }
 ]
 
